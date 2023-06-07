7c8b44029477911a6e9815685a95b1020ecb42c2
SuperDev <supernova@supers0ft.us>
2023-06-07 06:39:38 -0500

    Fixes

--- newtab.js
+++ newtab.js
@@ -1,82 +1,83 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var quoteElement = document.querySelector(".quote");
-    var timeElement = document.querySelector(".time");
-    var weatherElement = document.querySelector(".weather");
-  
-    // Fetch the quote
-    fetchQuote();
-  
-    // Display the current time
-    displayTime();
-  
-    // Get the weather information
-    getWeather();
-  
-    function fetchQuote() {
-        fetch('https://zenquotes-proxy.supernova3339.workers.dev/').then(response =>
-          response.json()
-        ).then(data => {
-          var quote = data[0].q;
-          var author = data[0].a;
-      
-          var quoteText = `"${quote}" - ${author}`;
-          quoteElement.textContent = quoteText;
-        }).catch(error => {
-          console.log("Error fetching quote:", error);
-          quoteElement.innerHTML = "Failed to fetch quote.";
-        });
-      }
-      
-      
-  
-    function displayTime() {
-      var currentTime = new Date();
-      var hours = currentTime.getHours();
-      var minutes = currentTime.getMinutes();
-      var ampm = hours >= 12 ? "PM" : "AM";
-  
-      hours = hours % 12 || 12;
-      minutes = minutes < 10 ? "0" + minutes : minutes;
-  
-      var timeText = hours + ":" + minutes + " " + ampm;
-      timeElement.textContent = timeText;
-    }
-  
-    function getWeather() {
-  var apiKey = localStorage.getItem("apiKey");
-  var country = localStorage.getItem("country");
-  var state = localStorage.getItem("state");
-  var city = localStorage.getItem("city");
-  var temperatureUnit = localStorage.getItem("temperatureUnit");
+  var quoteElement = document.querySelector(".quote");
+  var timeElement = document.querySelector(".time");
+  var weatherElement = document.querySelector(".weather");
 
-  if (!apiKey || !country || !state || !city) {
-    weatherElement.innerHTML = "Please provide an API Key, Country, State, and City in the options.";
-    return;
+  // Fetch the quote
+  fetchQuote();
+
+  // Display the current time
+  displayTime();
+
+  // Get the weather information
+  getWeather();
+
+  function fetchQuote() {
+    fetch('https://zenquotes-proxy.supernova3339.workers.dev/')
+      .then(response => response.json())
+      .then(data => {
+        var quote = data[0].q;
+        var author = data[0].a;
+
+        var quoteText = `"${quote}" - ${author}`;
+        quoteElement.textContent = quoteText;
+      })
+      .catch(error => {
+        console.log("Error fetching quote:", error);
+        quoteElement.innerHTML = "Failed to fetch quote.";
+      });
   }
 
-  var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&appid=${apiKey}&units=metric`;
+  function displayTime() {
+    var currentTime = new Date();
+    var hours = currentTime.getHours();
+    var minutes = currentTime.getMinutes();
+    var ampm = hours >= 12 ? "PM" : "AM";
 
-  fetch(apiUrl)
-    .then(response => response.json())
-    .then(data => {
-      var temperature = data.main.temp;
-      var temperatureSymbol = temperatureUnit === "fahrenheit" ? "°F" : "°C";
+    hours = hours % 12 || 12;
+    minutes = minutes < 10 ? "0" + minutes : minutes;
 
-      if (temperatureUnit === "fahrenheit") {
-        temperature = (temperature * 9 / 5) + 32;
+    var timeText = hours + ":" + minutes + " " + ampm;
+    timeElement.textContent = timeText;
+  }
+
+  function getWeather() {
+    chrome.storage.sync.get(
+      ["apiKey", "country", "state", "city", "temperatureUnit"],
+      function(result) {
+        var apiKey = result.apiKey;
+        var country = result.country;
+        var state = result.state;
+        var city = result.city;
+        var temperatureUnit = result.temperatureUnit;
+
+        if (!apiKey || !country || !state || !city) {
+          weatherElement.innerHTML = "Please provide an API Key, Country, State, and City in the options.";
+          return;
+        }
+
+        var apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city},${state},${country}&appid=${apiKey}&units=metric`;
+
+        fetch(apiUrl)
+          .then(response => response.json())
+          .then(data => {
+            var temperature = data.main.temp;
+            var temperatureSymbol = temperatureUnit === "fahrenheit" ? "°F" : "°C";
+
+            if (temperatureUnit === "fahrenheit") {
+              temperature = (temperature * 9 / 5) + 32;
+            }
+
+            temperature = temperature.toFixed(1);
+
+            var weatherText = `Current temperature: ${temperature}${temperatureSymbol}`;
+            weatherElement.textContent = weatherText;
+          })
+          .catch(error => {
+            console.log("Error fetching weather:", error);
+            weatherElement.innerHTML = "Failed to fetch weather data.";
+          });
       }
-
-      temperature = temperature.toFixed(1);
-
-      var weatherText = `Current temperature: ${temperature}${temperatureSymbol}`;
-      weatherElement.textContent = weatherText;
-    })
-    .catch(error => {
-      console.log("Error fetching weather:", error);
-      weatherElement.innerHTML = "Failed to fetch weather data.";
-    });
-}
-
-      
-  });
-  
+    );
+  }
+});
--- options.js
+++ options.js
@@ -1,46 +1,45 @@
 document.addEventListener("DOMContentLoaded", function() {
-    var optionsForm = document.getElementById("options-form");
-    var apiKeyInput = document.getElementById("apiKey");
-    var countryInput = document.getElementById("country");
-    var stateInput = document.getElementById("state");
-    var cityInput = document.getElementById("city");
-    var temperatureUnitInput = document.getElementById("temperatureUnit");
-  
-    loadOptions();
-  
-    optionsForm.addEventListener("submit", function(event) {
-      event.preventDefault();
-      saveOptions();
-    });
-  
-    function loadOptions() {
-      var apiKey = localStorage.getItem("apiKey");
-      var country = localStorage.getItem("country");
-      var state = localStorage.getItem("state");
-      var city = localStorage.getItem("city");
-      var temperatureUnit = localStorage.getItem("temperatureUnit");
-  
-      apiKeyInput.value = apiKey;
-      countryInput.value = country;
-      stateInput.value = state;
-      cityInput.value = city;
-      temperatureUnitInput.value = temperatureUnit;
-    }
-  
-    function saveOptions() {
-      var apiKey = apiKeyInput.value;
-      var country = countryInput.value;
-      var state = stateInput.value;
-      var city = cityInput.value;
-      var temperatureUnit = temperatureUnitInput.value;
-  
-      localStorage.setItem("apiKey", apiKey);
-      localStorage.setItem("country", country);
-      localStorage.setItem("state", state);
-      localStorage.setItem("city", city);
-      localStorage.setItem("temperatureUnit", temperatureUnit);
-  
-      alert("Options saved successfully!");
-    }
+  var optionsForm = document.getElementById("options-form");
+  var apiKeyInput = document.getElementById("apiKey");
+  var countryInput = document.getElementById("country");
+  var stateInput = document.getElementById("state");
+  var cityInput = document.getElementById("city");
+  var temperatureUnitInput = document.getElementById("temperatureUnit");
+
+  loadOptions();
+
+  optionsForm.addEventListener("submit", function(event) {
+    event.preventDefault();
+    saveOptions();
   });
-  
+
+  function loadOptions() {
+    var apiKey = localStorage.getItem("apiKey");
+    var country = localStorage.getItem("country");
+    var state = localStorage.getItem("state");
+    var city = localStorage.getItem("city");
+    var temperatureUnit = localStorage.getItem("temperatureUnit");
+
+    apiKeyInput.value = apiKey;
+    countryInput.value = country;
+    stateInput.value = state;
+    cityInput.value = city;
+    temperatureUnitInput.value = temperatureUnit;
+  }
+
+  function saveOptions() {
+    var apiKey = apiKeyInput.value;
+    var country = countryInput.value;
+    var state = stateInput.value;
+    var city = cityInput.value;
+    var temperatureUnit = temperatureUnitInput.value;
+
+    localStorage.setItem("apiKey", apiKey);
+    localStorage.setItem("country", country);
+    localStorage.setItem("state", state);
+    localStorage.setItem("city", city);
+    localStorage.setItem("temperatureUnit", temperatureUnit);
+
+    alert("Options saved successfully!");
+  }
+});
