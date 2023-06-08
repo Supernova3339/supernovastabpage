572a1d867b1efc7f55b697cd722598b3398fc471
SuperDev <supernova@supers0ft.us>
2023-06-08 08:21:12 -0500

    1.0.3 Changes and updates
    Allow for setting a background color and text color

--- options.js
+++ options.js
@@ -2,6 +2,8 @@ document.addEventListener("DOMContentLoaded", function() {
   var optionsForm = document.getElementById("options-form");
   var apiKeyInput = document.getElementById("apiKey");
   var temperatureUnitInput = document.getElementById("temperatureUnit");
+  var wallpaperColorInput = document.getElementById("wallpaperColor");
+  var textColorInput = document.getElementById("textColor");
 
   loadOptions();
 
@@ -13,17 +15,25 @@ document.addEventListener("DOMContentLoaded", function() {
   function loadOptions() {
     var apiKey = localStorage.getItem("apiKey");
     var temperatureUnit = localStorage.getItem("temperatureUnit");
+    var wallpaperColor = localStorage.getItem("wallpaperColor");
+    var textColor = localStorage.getItem("textColor");
 
     apiKeyInput.value = apiKey;
     temperatureUnitInput.value = temperatureUnit;
+    wallpaperColorInput.value = wallpaperColor || "#000000";
+    textColorInput.value = textColor || "#ffffff";
   }
 
   function saveOptions() {
     var apiKey = apiKeyInput.value;
     var temperatureUnit = temperatureUnitInput.value;
+    var wallpaperColor = wallpaperColorInput.value;
+    var textColor = textColorInput.value;
 
     localStorage.setItem("apiKey", apiKey);
     localStorage.setItem("temperatureUnit", temperatureUnit);
+    localStorage.setItem("wallpaperColor", wallpaperColor);
+    localStorage.setItem("textColor", textColor);
 
     alert("Options saved successfully!");
   }
